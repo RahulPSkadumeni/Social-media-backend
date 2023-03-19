549e8c8b2abb95eb0a41bcb3044572d45c387727
Rahul P S <rahulps995@gmail.com>
2023-03-20 02:13:34 +0530

    20/3/2023 mng  add like dislike timeLine post

--- routes/posts.js
+++ routes/posts.js
@@ -113,21 +113,6 @@ router.get("/timeline/all", async (req, res) => {
   }
 });
 
-// router.put("/:id/like", async (req, res) => {
-//   try {
-//     const post = await Post.findById(req.params.id);
-//     if (!post.likes.includes(req.body.userId)) {
-//       await post.updateOne({ $push: { likes: req.body.userId } });
-//       res.status(200).json("The post has been liked");
-//     } else {
-//       await post.updateOne({ $pull: { likes: req.body.userId } });
-//       res.status(200).json("The post has been disliked");
-//     }
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
 // unlike a post
 //get a post
 //get all post of following(timeline)
